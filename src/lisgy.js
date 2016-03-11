cb238b2e039fa5bf6b35eef94ada8b592dcc545b
Alexander Treptau <atreptau@welfenlab.de>
2016-03-11 18:00:26 +0100

    Parse lisp ast to edges and nodes

--- src/lisgy.js
+++ src/lisgy.js
@@ -1,6 +1,87 @@
 var tokenizer = require('wsl-lisp-parser')
 
-export function parse (code) {
+export function parse (code, options) {
+  var config = options || {addDepth: true, addCalls: true}
   var root = tokenizer.parseToAst(code, '')
-  return root
+  var data = {nodes: [], edges: []}
+
+  function Node (name, depth) {
+    if (config.addDepth) {
+      return {name: name, depth: depth}
+    }
+    return {name: name}
+  }
+  function Edge (from, to) {
+    return {from: from, to: to}
+  }
+
+  var calls = 0
+  function parse (root, data, depth, parrent) {
+    if (root.type !== 'AstApply' || !root.data) return
+    calls++
+
+    function newEdge (from, to) {
+      var edge = new Edge(from, to)
+      if (config.addCalls) {
+        edge.calls = calls
+      }
+      data.edges.push(edge)
+      return edge
+    }
+
+    function newNode (name, depth) {
+      var node = new Node(name, depth)
+      if (config.addCalls) {
+        node.calls = calls
+        data.nodes.push(node)
+      } else {
+        if (!data.nodes.some(elem => elem.name === name)) {
+          data.nodes.push(node)
+        }
+      }
+      return node
+    }
+
+    var nextDepth = depth + 1
+    var baseValue = root.data[0].value
+    var baseObj = newNode(baseValue, depth)
+    newEdge(parrent, baseObj)
+
+    if (baseValue === 'lambda') {
+      baseObj.data = {nodes: [], edges: []}
+      var vars = root.data[1]
+      var lambdaVars = []
+      for (var i = 0; i < vars.data.length; i++) {
+        lambdaVars.push(vars.data[i].value)
+      }
+      baseObj.vars = lambdaVars
+
+      var fnc = root.data[2]
+      baseObj.fnc = fnc
+
+      if (fnc.type !== 'AstApply') {
+        console.log('Error')
+      }
+
+      parse(fnc, baseObj.data, nextDepth, 'lambda')
+      return
+    }
+
+    for (var j = 1; j < root.data.length; j++) {
+      var args = root.data[j]
+      switch (args.type) {
+        case 'AstAtom':
+        case 'AstNumber':
+        case 'AstString':
+          newEdge(baseObj, newNode(args.value, nextDepth))
+          break
+        default:
+          parse(args, data, nextDepth, baseObj)
+          break
+      }
+    }
+  }
+
+  parse(root.data[0], data, 0, 'root')
+  return data
 }
--- test/lisgy.js
+++ test/lisgy.js
@@ -1,14 +1,82 @@
 /* global describe, it */
 
 var expect = require('chai').expect
-var parse = require('../src/lisgy.js').parse
+var lisgy = require('../src/lisgy.js')
+
+var compare = function (edges, id, from, to) {
+  expect(edges[id].from).to.have.property('name').to.equal(from)
+  expect(edges[id].to).to.have.property('name').to.equal(to)
+}
 
 describe('Parser', function () {
-  it('T0', function () {
-    var parsed = parse("(lambda (x y) (+ x y))")
-    //console.log(parsed)
-    /*
-    expect(curGraph).to.deep.equal(cmpGraph)
-    */
-  })
+  (function () {
+    var code = '(lambda (x y) (> x y))'
+    var parsed
+    var lambda, nodes, edges
+
+    it('can parse ' + code, function () {
+      parsed = lisgy.parse(code, {addDepth: true, addCalls: true})
+
+      expect(parsed.nodes.length).to.equal(1)
+    })
+
+    it('has variables', function () {
+      lambda = parsed.nodes[0]
+      expect(lambda.name).to.equal('lambda')
+      expect(lambda.vars).to.have.members([ 'x', 'y' ])
+    })
+
+    it('has the right number of nodes and edges', function () {
+      nodes = lambda.data.nodes
+      edges = lambda.data.edges
+
+      expect(nodes.length).to.equal(3)
+      expect(edges.length).to.equal(3)
+    })
+
+    it('has the correct edges', function () {
+      expect(edges[0].to).to.have.property('name').to.equal('>')
+
+      compare(edges, 1, '>', 'x')
+      compare(edges, 2, '>', 'y')
+
+    })
+
+  })()
+
+  ;(function () {
+    var code = '(lambda (x y z) (> (+ x y) (+ z y)))'
+    var parsed
+    var lambda, nodes, edges
+
+    it('can parse ' + code, function () {
+      parsed = lisgy.parse(code, {addDepth: true, addCalls: true})
+
+      expect(parsed.nodes.length).to.equal(1)
+    })
+
+    it('has variables', function () {
+      lambda = parsed.nodes[0]
+      expect(lambda.name).to.equal('lambda')
+      expect(lambda.vars).to.have.members([ 'x', 'y', 'z' ])
+    })
+
+    it('has the right number of nodes and edges', function () {
+      nodes = lambda.data.nodes
+      edges = lambda.data.edges
+
+      expect(nodes.length).to.equal(7)
+      expect(edges.length).to.equal(7)
+    })
+
+    it('has the correct edges', function () {
+      expect(edges[0].to).to.have.property('name').to.equal('>')
+      compare(edges, 1, '>', '+')
+      compare(edges, 2, '+', 'x')
+      compare(edges, 3, '+', 'y')
+      compare(edges, 4, '>', '+')
+      compare(edges, 5, '+', 'z')
+      compare(edges, 6, '+', 'y')
+    })
+  })()
 })
