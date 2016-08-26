6dffebd91001748e0b8774b9a6502b36bd367692
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-08-26 15:15:48 +0200

    (style) Fix linter errors.

--- src/functions/defco.js
+++ src/functions/defco.js
@@ -1,8 +1,8 @@
 export default function (ednObject, { context, compile }) {
   // TODO create and return { graph, port }
-  return {
+  /* return {
     context, // new context (with new deco'ed component)
     node, // created node
     outputPort: 'output' // output port for next component (not applicable for defco)
-  }
+  } */
 }
