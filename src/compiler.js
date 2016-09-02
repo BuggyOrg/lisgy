0868e9494fedd49c28da5c890b85e32f7504d9e0
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-09-02 12:10:43 +0200

    (feat) Return the new context and the new graph in compile functions.

--- src/compiler.js
+++ src/compiler.js
@@ -1,30 +1,48 @@
 import _ from 'lodash'
+import * as Graph from '@buggyorg/graphtools'
 import * as functions from './functions'
+
+function ifDefinedOr(value, )
 
 function getFunctionHandler (name) {
   return functions[name] || functions.externalComponent
 }
 
-function compileWithContext (ednObj, context) {
+function compileWithContext (ednObj, context, graph) {
   if (_.isArray(ednObj.val)) {
     if (_.isString(ednObj.val[0].val)) {
       const fn = getFunctionHandler(ednObj.val[0].val)
       const result = fn(ednObj, { context, compile: compileWithContext })
-      return { context: result.context }
+      if (result) {
+        return { context: result.context || context, graph: result.graph || graph }
+      } else {
+        return { context, graph }
+      }
     } else {
-      let currentContext = context
+      let current = { context, graph }
       ednObj.val.forEach((v) => {
-        const result = compileWithContext(v, currentContext)
-        currentContext = result.context
+        const newCurrent = compileWithContext(v, current.context, current.graph)
+        if (newCurrent) {
+          if (newCurrent.context) {
+            current.context = newCurrent.context
+          }
+          if (newCurrent.graph) {
+            current.graph = newCurrent.graph
+          }
+        }
       })
-      return { context: currentContext }
+      return current
     }
   } else {
-    return { context }
+    return { context, graph }
   }
 }
 
 export function compile (ednObj) {
-  const context = { modules: {}, variables: {}, count: 0 }
-  return compileWithContext(ednObj, context)
+  const context = {
+    modules: {},
+    variables: {},
+    count: 0
+  }
+  return compileWithContext(ednObj, context, Graph.empty()).graph
 }
