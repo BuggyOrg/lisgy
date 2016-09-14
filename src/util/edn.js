8c17aab795bba435fd1f344a169e62467044bfb6
Alexander Treptau <atreptau@welfenlab.de>
2016-09-14 16:59:21 +0200

    (fix) Add missing src/util/end.js.

---
+++ src/util/edn.js
@@ -0,0 +1,18 @@
+import * as edn from 'jsedn'
+import { cleanPort } from './graph'
+
+export function isInfoObject (ednObject) {
+  return ednObject && ednObject.keys
+}
+
+export function extraInfosAdded (cmpt, ednObject) {
+  if (isInfoObject(ednObject)) {
+    let keys = ednObject.keys
+    let vals = ednObject.vals
+    keys.forEach((data, i) => {
+      cmpt[ cleanPort(data.toString()) ] = edn.toJS(vals[i])
+    })
+    return true
+  }
+  return false
+}
