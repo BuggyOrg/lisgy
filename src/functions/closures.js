99cb906a25adf7e6a1162e8b4bff8cd3aec16ddf
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-11-04 15:31:13 +0100

    (feat) Handle unused and deeply used variables in closures properly.

--- src/functions/closures.js
+++ src/functions/closures.js
@@ -1,30 +1,65 @@
 import _ from 'lodash'
 
+function getUsedVariables (ednObject) {
+  let vars = []
+
+  if (_.isArray(ednObject)) {
+    ednObject.forEach((obj) => {
+      Array.prototype.push.apply(vars, getUsedVariables(obj))
+    })
+  } else {
+    const val = ednObject.val
+    // val is a function inside the lambda function
+
+    if (Array.isArray(val)) {
+      // assumption: val[0] is not a variable
+      if (val[0].val === 'lambda') {
+        Array.prototype.push.apply(vars, getUsedVariables(val[0]))
+      } else {
+        for (let j = 1; j < val.length; j++) {
+          if (val[j].name) {
+            vars.push(val[j].name)
+          } else if (_.isObject(val[j].val)) {
+            Array.prototype.push.apply(vars, getUsedVariables(val[j]))
+          }
+        }
+      }
+    } else if (_.isObject(val) && val.name) {
+      vars.push(val.name)
+    }
+  }
+
+  return vars
+}
+
 export function transformClosures (ednObject, knownVariables) {
   const lambdaArgs = ednObject.val[1].val.map(({ name }) => name)
-  const boundArgs = _.without(knownVariables, lambdaArgs)
-  // TODO remove bound arguments that are unused in the closure
+  const boundArgs = _.intersection(_.without(knownVariables, lambdaArgs), getUsedVariables(ednObject.val.slice(2)))
 
-  let newLambda = _.cloneDeep(ednObject)
-  newLambda.val[1].val = _.concat(boundArgs.map((boundArg) => ({
-    ns: null,
-    name: boundArg,
-    val: boundArg
-  })), newLambda.val[1].val)
+  if (boundArgs.length === 0) {
+    return ednObject
+  } else {
+    let newLambda = _.cloneDeep(ednObject)
+    newLambda.val[1].val = _.concat(boundArgs.map((boundArg) => ({
+      ns: null,
+      name: boundArg,
+      val: boundArg
+    })), newLambda.val[1].val)
 
-  boundArgs.forEach((boundArg) => {
-    newLambda = {
-      val: [{
-        ns: null,
-        name: 'partial',
-        val: 'partial'
-      }, newLambda, {
-        ns: null,
-        name: boundArg,
-        val: boundArg
-      }]
-    }
-  })
+    boundArgs.forEach((boundArg) => {
+      newLambda = {
+        val: [{
+          ns: null,
+          name: 'partial',
+          val: 'partial'
+        }, newLambda, {
+          ns: null,
+          name: boundArg,
+          val: boundArg
+        }]
+      }
+    })
 
-  return newLambda
+    return newLambda
+  }
 }
--- test/functions/closures.js
+++ test/functions/closures.js
@@ -5,7 +5,7 @@ import { parse } from '../../src/parser'
 import { transformClosures } from '../../src/functions/closures'
 
 describe.only('closures', () => {
-  it('should transform closures', () => {
+  it('should bind variables in closures', () => {
     const anonymousFunction = parse('(let [b 42] (lambda [a] (add a b)))').val[0]
     const lambdaFunction = _.last(anonymousFunction.val)
     const transformed = transformClosures(lambdaFunction, ['b'])
@@ -14,5 +14,36 @@ describe.only('closures', () => {
     expect(transformed.val[0].val).to.equal('partial')
     expect(transformed.val[1].val[1].val.map(({ name }) => name)).to.deep.equal(['b', 'a'])
     expect(transformed.val[2].val).to.equal('b')
+  })
+
+  it('should bind variables in closures when they are used deeply', () => {
+    const anonymousFunction = parse('(let [b 42] (lambda [a] (add a (add b 2))))').val[0]
+    const lambdaFunction = _.last(anonymousFunction.val)
+    const transformed = transformClosures(lambdaFunction, ['b'])
+
+    // binds b to the lambda function
+    expect(transformed.val[0].val).to.equal('partial')
+    expect(transformed.val[1].val[1].val.map(({ name }) => name)).to.deep.equal(['b', 'a'])
+    expect(transformed.val[2].val).to.equal('b')
+  })
+
+  it('should not bind unused variables in closures', () => {
+    const anonymousFunction = parse('(let [b 42] (lambda [a] (add a 1)))').val[0]
+    const lambdaFunction = _.last(anonymousFunction.val)
+    const transformed = transformClosures(lambdaFunction, ['b'])
+
+    // does not bind unused variable b to the lambda function
+    expect(transformed.val[0].val).to.equal('lambda')
+    expect(transformed.val[1].val.map(({ name }) => name)).to.deep.equal(['a'])
+  })
+
+  it('should not bind shadowed variables in closures', () => {
+    const anonymousFunction = parse('(let [b 42] (lambda [a] (lambda [b] (math/add a b))))').val[0]
+    const lambdaFunction = _.last(anonymousFunction.val)
+    const transformed = transformClosures(lambdaFunction, ['b'])
+
+    // does not bind unused variable b to the lambda function
+    expect(transformed.val[0].val).to.equal('lambda')
+    expect(transformed.val[1].val.map(({ name }) => name)).to.deep.equal(['a'])
   })
 })
