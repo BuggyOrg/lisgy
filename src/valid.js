d29da0589739aad5e3e84a220c2dc87c5dc66805
Alexander Treptau <atreptau@welfenlab.de>
2016-12-12 15:34:34 +0100

    (feature) Add simple valid lisgy code check tool.

---
+++ src/valid.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { parse } from './parser'
+
+try {
+  parse(process.argv[process.argv.length - 1])
+} catch (err) {
+  console.log('ERROR:', err.message)
+  process.exit(1)
+}
