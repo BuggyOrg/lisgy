88340010685e2e04e31eac8b4fbf3f162bb89315
Alexander Treptau <atreptau@welfenlab.de>
2016-12-16 18:30:38 +0100

    (feature) Add two simple cli tests.

---
+++ test/cli.js
@@ -0,0 +1,52 @@
+
+/* global describe, it */
+import * as Graph from '@buggyorg/graphtools'
+import {exec} from 'child_process'
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
+const expect = chai.expect
+chai.use(chaiAsPromised)
+
+const runCLI = (args, data) => {
+  return new Promise((resolve, reject) => {
+    var cli = exec('node lib/cli ' + args,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(stderr)
+        } else {
+          resolve(stdout)
+        }
+      }
+    )
+    if (data) {
+      if (typeof data !== 'string') {
+        data = JSON.stringify(data)
+      }
+      cli.stdin.write(data)
+      cli.stdin.end()
+    }
+  })
+}
+
+describe('CLI Commands', () => {
+  describe('`pc [code]` -> parse and compile the code', () => {
+    it('can use a simple arg string', () => {
+      let a = runCLI('pc "(defco test [a b] (+ a b))"')
+      return expect(a).to.be.fulfilled
+    })
+  })
+
+  describe('`input [file]` or `i [file]` -> process the stdin as lisgy code', () => {
+    it('can use a simple input string', () => {
+      let p = runCLI('i', '(defco test [a b] (+ a b))').then((a) => JSON.parse(a))
+      expect(p).to.be.fulfilled
+      return p.then((a) => {
+        let g = Graph.fromJSON(a)
+        expect(Graph.nodes(g)).to.have.length(0)
+        expect(Graph.edges(g)).to.have.length(0)
+        expect(Graph.components(g)).to.have.length(1)
+      })
+    })
+  })
+})
